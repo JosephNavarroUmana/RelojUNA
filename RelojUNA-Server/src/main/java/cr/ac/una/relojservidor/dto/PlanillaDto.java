55ff214861cc783cebf45f1d3816277cf170d6e0
Joseph Navarro <josephnavauma@gmail.com>
2026-08-12 14:52:44 -0600

    Clases de webService esqueleto

---
+++ RelojUNA-Server/src/main/java/cr/ac/una/relojservidor/dto/PlanillaDto.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package cr.ac.una.relojservidor.dto;
+
+/**
+ *
+ * @author Usuario
+ */
+public class PlanillaDto {
+    
+}
